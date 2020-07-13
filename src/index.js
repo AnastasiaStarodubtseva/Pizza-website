104a461f83181d9f8dae85e96a882a4eb7595726
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-07-13 18:28:13 +0300

    Add basket page, link to the button for the basket page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import Header from './components/Header';
 import './scss/app.scss';
 import App from './App';
@@ -8,10 +8,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode >
-    <BrowserRouter>
-      <Route exact path="/" component={App} />
-      <Route exact path="/qwe" component={Header} />
-    </BrowserRouter>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
